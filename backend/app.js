9171d9d6830056c53dcd3b714aa9a4754851b505
Rodolfo Román <rodolforoman@Rodolfos-MacBook-Pro.local>
2018-05-15 21:32:35 -0500

    Backend, remove static resources/routes

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const cookieParser = require( 'cookie-parser' )
 const logger = require( 'morgan' )
 
 const indexRouter = require( './routes/index' )
-const usersRouter = require( './routes/users' )
 
 const app = express()
 
@@ -12,9 +11,7 @@ app.use( logger( 'dev' ) )
 app.use( express.json() )
 app.use( express.urlencoded( { extended: false } ) )
 app.use( cookieParser() )
-app.use( express.static( path.join( __dirname, 'public' ) ) )
 
 app.use( '/', indexRouter )
-app.use( '/users', usersRouter )
 
 module.exports = app
