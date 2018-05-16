472d884cb51f54ebecc7c971790af969398aba0e
Rodolfo Román <rromanv@gmail.com>
2018-05-15 23:55:37 -0500

    Custom Broadcast in Backend

--- backend/sockets/base.js
+++ backend/sockets/base.js
@@ -1,6 +1,14 @@
 const socketIO = io => {
   io.on( 'connection',  socket => {
     console.log( 'New user Connected: ' + socket.id )
+    socket.on( 'increment', ( counter ) => {
+      console.log( 'increment' )
+      io.sockets.emit( 'COUNTER_INCREMENT', counter + 1 )
+    } )
+    socket.on( 'decrement', ( counter ) => {
+      console.log( 'decrement' )
+      io.sockets.emit( 'COUNTER_DECREMENT', counter - 1 )
+    } )
   } )
 }
 
