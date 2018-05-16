3284398c93b1f5f13ba33660ff5b81b67f7ca57e
Rodolfo Román <rodolforoman@Rodolfos-MacBook-Pro.local>
2018-05-15 22:04:42 -0500

    Basic Vuex State Management

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,18 @@ Vue.use( Vuex )
 
 export default new Vuex.Store( {
   state: {
-
+    count: 0,
   },
   mutations: {
-
+    increment ( state ) {
+      state.count++
+    },
+    decrement ( state ) {
+      state.count--
+    },
   },
   actions: {
-
+    increment: ( { commit } ) => commit( 'increment' ),
+    decrement: ( { commit } ) => commit( 'decrement' ),
   },
 } )
