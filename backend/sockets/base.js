b73fc413c357e413a73dae977dea2d58b92db6fe
Rodolfo Román <rromanv@gmail.com>
2018-05-16 00:02:10 -0500

    Typo

--- backend/sockets/base.js
+++ backend/sockets/base.js
@@ -2,11 +2,11 @@ const socketIO = io => {
   io.on( 'connection',  socket => {
     console.log( 'New user Connected: ' + socket.id )
     socket.on( 'increment', ( counter ) => {
-      console.log( 'increment' )
+      console.log( `Increment from ${socket.id}` )
       io.sockets.emit( 'COUNTER_INCREMENT', counter + 1 )
     } )
     socket.on( 'decrement', ( counter ) => {
-      console.log( 'decrement' )
+      console.log( `Decrement from ${socket.id}` )
       io.sockets.emit( 'COUNTER_DECREMENT', counter - 1 )
     } )
   } )
